c28d23c8f4963849a2e33c378a3c868f0508a843
lanaro0108 <pedro.lanaro@aluno.senai.br>
2025-12-21 11:59:00 -0300

    Commit inicial do Projeto WorldSkills "Dia01"

---
+++ src/exercicios/dia01/ex01/Exercicio01.java
@@ -0,0 +1,20 @@
+package exercicios.dia01.ex01;
+
+public class Exercicio01 {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+/*
+psvm = public static void main (String[] args) {}
+sout = System.out.println();
+
+Java é case sensitive
+
+classes = MinhaClasse
+atributos = meuAtributo
+variável = minhaVariável
+métodos = meuMetodo
+pacotes = meu_pacote
+constante = CONSTANTE
+*/
