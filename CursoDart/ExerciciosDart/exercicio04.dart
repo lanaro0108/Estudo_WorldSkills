51fd39754c2559ffb09f33eee0c0ccfa6da9d384
lanaro0108 <pedro.lanaro@aluno.senai.br>
2026-01-05 15:50:00 -0300

    Add ejercicio04.dart with loop exercises

---
+++ CursoDart/ExerciciosDart/exercicio04.dart
@@ -0,0 +1,20 @@
+// Exercícios de Laços de repetição
+
+// Mostre os números de 1 a 10 usando for.
+void main() {
+  for (int i = 1; i < 11; i++) {
+    print(i);
+  }
+
+  // Mostre apenas os números pares de 1 a 20
+  for (int i = 1; i < 11; i++) {
+    print(i * 2);
+  }
+
+  //Use while para contar de 10 até 1.
+  int i = 10;
+  while (i >= 1) {
+    print(i);
+    i--;
+  }
+}
