1cc2d6d9f9bcccc315f0119367e09bc2111e8988
lanaro0108 <pedro.lanaro@aluno.senai.br>
2026-01-05 15:48:59 -0300

    Add variable exercises in exercicio01.dart

--- CursoDart/ExerciciosDart/exercicio01.dart
+++ CursoDart/ExerciciosDart/exercicio01.dart
@@ -1,1 +1,24 @@
+// Exercícios de Variáveis
 
+void main() {
+
+// 1- Crie uma variável String com seu nome e imprima no console.
+  String nome = "Pedro";
+  print(nome);
+
+// 2- Crie duas variáveis int e mostre a soma delas.
+  int a = 5;
+  int b = 3;
+  int resultado = a + b;
+  print(resultado);
+
+// 3 - Crie uma variável double e outra int e multiplique as duas.
+  double num1 = 4.5;
+  int num2 = 8;
+  double resMult = num1 * num2;
+  print(resMult);
+
+// 4 - Crie uma variável bool chamada ligado e imprima seu valor.
+  bool ligado = true;
+  print(ligado);
+}
